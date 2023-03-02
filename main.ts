6eb8bc3ac5313a8337646d35de32ebbbc4732ac1
nakazawaken1 <nakazawaken1@gmail.com>
2023-03-02 14:20:39 +0000

    webgl2

--- main.ts
+++ main.ts
@@ -1,26 +1,38 @@
 (() => {
-    const view = { name: "T2202-03", x: 0, y: 0 };
-    function render(image: HTMLImageElement) {
+    const view = { h: 0, name: "T2202-03", x: 0, y: 0, z: 0, dragging: false };
+    const W = 512;
+    const H = 512;
+    const X = 17;
+    const Y = 7;
+    const SX = 0.1;
+    const SY = 0.2;
+    const load = async (url: string): Promise<HTMLImageElement> => new Promise(resolve => {
+        const image = new Image();
+        image.onload = () => resolve(image);
+        image.src = url;
+    });
+    async function render() {
         const canvas = document.querySelector("canvas");
         if (!canvas) throw "canvasが見つかりません";
         const range = document.querySelector("input");
         const value = document.querySelector("span");
-        const gl = canvas.getContext('webgl');
+        const gl = canvas.getContext('webgl2', { antialias: false });
         if (!gl) throw "WebGLが使えないため処理ができませんでした";
 
         const vertexShader = gl.createShader(gl.VERTEX_SHADER);
         if (!vertexShader) throw "バーテックスシェーダが作成できませんでした";
-        gl.shaderSource(vertexShader, `
-attribute vec2 aVertexPosition;
-attribute vec2 aTextureCoord;
-varying vec2 vTextureCoord;
+        gl.shaderSource(vertexShader, `#version 300 es
+in vec2 aVertexPosition;
+in vec2 aTextureCoord;
+out vec2 vTextureCoord;
 uniform vec2 uResolution;
+
 void main() {
-vec2 zeroToOne = aVertexPosition / uResolution;
-vec2 zeroToTwo = zeroToOne * 2.0;
-vec2 clipSpace = zeroToTwo - 1.0;
-gl_Position = vec4(clipSpace * vec2(1, -1), 0, 1);
-vTextureCoord = aTextureCoord;
+    vec2 zeroToOne = aVertexPosition / uResolution;
+    vec2 zeroToTwo = zeroToOne * 2.0;
+    vec2 clipSpace = zeroToTwo - 1.0;
+    gl_Position = vec4(clipSpace * vec2(1, -1), 0, 1);
+    vTextureCoord = aTextureCoord;
 }
 `);
         gl.compileShader(vertexShader);
@@ -32,34 +44,39 @@ vTextureCoord = aTextureCoord;
 
         const fragmentShader = gl.createShader(gl.FRAGMENT_SHADER);
         if (!fragmentShader) throw "フラグメントシェーダが作成できませんでした";
-        gl.shaderSource(fragmentShader, `
+        gl.shaderSource(fragmentShader, `#version 300 es
 precision mediump float;
+precision mediump sampler2DArray;
 
-varying vec2 vTextureCoord;
-uniform sampler2D uTexture;
+in vec2 vTextureCoord;
+out vec4 oFragColor;
+uniform sampler2DArray uTexture;
 uniform float uHue;
+uniform float uIndex;
 
 vec3 rgb2hsv(vec3 c)
 {
-vec4 K = vec4(0.0, -1.0 / 3.0, 2.0 / 3.0, -1.0);
-vec4 p = mix(vec4(c.bg, K.wz), vec4(c.gb, K.xy), step(c.b, c.g));
-vec4 q = mix(vec4(p.xyw, c.r), vec4(c.r, p.yzx), step(p.x, c.r));
+    vec4 K = vec4(0.0, -1.0 / 3.0, 2.0 / 3.0, -1.0);
+    vec4 p = mix(vec4(c.bg, K.wz), vec4(c.gb, K.xy), step(c.b, c.g));
+    vec4 q = mix(vec4(p.xyw, c.r), vec4(c.r, p.yzx), step(p.x, c.r));
 
-float d = q.x - min(q.w, q.y);
-float e = 1.0e-10;
-return vec3(abs(q.z + (q.w - q.y) / (6.0 * d + e)), d / (q.x + e), q.x);
+    float d = q.x - min(q.w, q.y);
+    float e = 1.0e-10;
+    return vec3(abs(q.z + (q.w - q.y) / (6.0 * d + e)), d / (q.x + e), q.x);
 }
+
 vec3 hsv2rgb(vec3 c)
 {
-vec4 K = vec4(1.0, 2.0 / 3.0, 1.0 / 3.0, 3.0);
-vec3 p = abs(fract(c.xxx + K.xyz) * 6.0 - K.www);
-return c.z * mix(K.xxx, clamp(p - K.xxx, 0.0, 1.0), c.y);
+    vec4 K = vec4(1.0, 2.0 / 3.0, 1.0 / 3.0, 3.0);
+    vec3 p = abs(fract(c.xxx + K.xyz) * 6.0 - K.www);
+    return c.z * mix(K.xxx, clamp(p - K.xxx, 0.0, 1.0), c.y);
 }
+
 void main(void) {
-vec4 color = texture2D(uTexture, vTextureCoord);
-vec3 hsv = rgb2hsv(color.rgb);
-hsv.x = mod(hsv.x + uHue, 1.0);
-gl_FragColor = vec4(hsv2rgb(hsv), 1.0);
+    vec4 color = texture(uTexture, vec3(vTextureCoord, uIndex));
+    vec3 hsv = rgb2hsv(color.rgb);
+    hsv.x = mod(hsv.x + uHue, 1.0);
+    oFragColor = vec4(hsv2rgb(hsv), 1.0);
 }
 `);
         gl.compileShader(fragmentShader);
@@ -78,20 +95,22 @@ gl_FragColor = vec4(hsv2rgb(hsv), 1.0);
             throw 'プログラムを初期化できませんでした：' + gl.getProgramInfoLog(program);
         }
 
-        const vertexPosition = gl.getAttribLocation(program, 'aVertexPosition');
-        const textureCoord = gl.getAttribLocation(program, 'aTextureCoord');
-        const resolution = gl.getUniformLocation(program, "uResolution");
-        const hue = gl.getUniformLocation(program, "uHue");
+        const aVertexPosition = gl.getAttribLocation(program, 'aVertexPosition');
+        const aTextureCoord = gl.getAttribLocation(program, 'aTextureCoord');
+        const uResolution = gl.getUniformLocation(program, "uResolution");
+        const uHue = gl.getUniformLocation(program, "uHue");
+        const uIndex = gl.getUniformLocation(program, "uIndex");
+        const uTexture = gl.getUniformLocation(program, "uTexture");
 
         const positionBuffer = gl.createBuffer();
         gl.bindBuffer(gl.ARRAY_BUFFER, positionBuffer);
         gl.bufferData(gl.ARRAY_BUFFER, new Float32Array([
             0.0, 0.0,
-            image.width, 0.0,
-            0.0, image.height,
-            0.0, image.height,
-            image.width, 0.0,
-            image.width, image.height]), gl.STATIC_DRAW);
+            W, 0.0,
+            0.0, H,
+            0.0, H,
+            W, 0.0,
+            W, H]), gl.STATIC_DRAW);
 
         const textureCoordBuffer = gl.createBuffer();
         gl.bindBuffer(gl.ARRAY_BUFFER, textureCoordBuffer);
@@ -105,46 +124,97 @@ gl_FragColor = vec4(hsv2rgb(hsv), 1.0);
 
         // 「テクスチャー」を生成する
         const texture = gl.createTexture();
-        gl.bindTexture(gl.TEXTURE_2D, texture);
-
+        gl.activeTexture(gl.TEXTURE0);
+        gl.bindTexture(gl.TEXTURE_2D_ARRAY, texture);
+        gl.texStorage3D(
+            gl.TEXTURE_2D_ARRAY,
+            Math.floor(Math.log(W) / Math.log(2)) + 1,
+            gl.RGBA8,
+            W,
+            H,
+            X * Y
+        );
         // どんなサイズの画像でもレンダリングできるようにパラメータを設定する
-        gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, gl.CLAMP_TO_EDGE);
-        gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, gl.CLAMP_TO_EDGE);
-        gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.NEAREST);
-        gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.NEAREST);
+        gl.texParameteri(gl.TEXTURE_2D_ARRAY, gl.TEXTURE_WRAP_S, gl.CLAMP_TO_EDGE);
+        gl.texParameteri(gl.TEXTURE_2D_ARRAY, gl.TEXTURE_WRAP_T, gl.CLAMP_TO_EDGE);
+        gl.texParameteri(gl.TEXTURE_2D_ARRAY, gl.TEXTURE_MIN_FILTER, gl.NEAREST);
+        gl.texParameteri(gl.TEXTURE_2D_ARRAY, gl.TEXTURE_MAG_FILTER, gl.NEAREST);
 
-        // テクスチャーに画像のデータをアップロードする
-        gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA, gl.RGBA, gl.UNSIGNED_BYTE, image);
+        //画像を読み込む
+        const temporaryCanvas = document.createElement('canvas');
+        temporaryCanvas.width = W;
+        temporaryCanvas.height = H;
+        const context = temporaryCanvas.getContext('2d');
+        if (!context) throw "2Dコンテクストが取得できません";
+        for (let x = 0; x < X; x++) {
+            for (let y = 0; y < Y; y++) {
+                const image = await load(`${view.name}/x-${Math.floor(y) % Y}_${view.name}_1.${X - Math.floor(x) % X}.png`);
+                context.drawImage(image, 0, 0);
+                gl.texSubImage3D(
+                    gl.TEXTURE_2D_ARRAY,
+                    0,
+                    0,
+                    0,
+                    x * Y + y,
+                    W,
+                    H,
+                    1,
+                    gl.RGBA,
+                    gl.UNSIGNED_BYTE,
+                    context.getImageData(0, 0, W, H)
+                );
+            }
+        }
 
         const draw = () => {
+            const hsv = rgb2hsv(255, 0, 0);
+            const changedHsv = { h: Math.floor(hsv.h + view.h * 360) % 360, s: hsv.s, v: hsv.v };
+            const rgb = hsv2rgb(changedHsv.h, changedHsv.s, changedHsv.v);
+            if (value) value.textContent = `H(${Number(range?.value).toFixed(2)}) RGB(${rgb.r}, ${rgb.g}, ${rgb.b}) x:${view.x.toFixed(1)} y:${view.y.toFixed(1)} z:${view.z.toFixed(1)}`;
+
             gl.canvas.width = canvas.clientWidth;
             gl.canvas.height = canvas.clientHeight;
-            gl.viewport(0, 0, gl.canvas.width, gl.canvas.height);
+            gl.viewport(view.z, view.z, gl.canvas.width - view.z, gl.canvas.height - view.z);
             gl.clearColor(0, 0, 0, 0);
             gl.clear(gl.COLOR_BUFFER_BIT);
             gl.useProgram(program);
 
-            gl.enableVertexAttribArray(vertexPosition);
+            gl.enableVertexAttribArray(aVertexPosition);
             gl.bindBuffer(gl.ARRAY_BUFFER, positionBuffer);
-            gl.vertexAttribPointer(vertexPosition, 2, gl.FLOAT, false, 0, 0);
+            gl.vertexAttribPointer(aVertexPosition, 2, gl.FLOAT, false, 0, 0);
 
-            gl.enableVertexAttribArray(textureCoord);
+            gl.enableVertexAttribArray(aTextureCoord);
             gl.bindBuffer(gl.ARRAY_BUFFER, textureCoordBuffer);
-            gl.vertexAttribPointer(textureCoord, 2, gl.FLOAT, false, 0, 0);
-            gl.uniform2f(resolution, image.width, image.height);
-            if (range) gl.uniform1f(hue, Number(range.value));
+            gl.vertexAttribPointer(aTextureCoord, 2, gl.FLOAT, false, 0, 0);
+            gl.uniform2f(uResolution, W, H);
+            gl.uniform1f(uIndex, view.x * Y + view.y);
+            gl.uniform1i(uTexture, 0);
+            if (range) gl.uniform1f(uHue, Number(range.value));
 
             gl.drawArrays(gl.TRIANGLES, 0, 6);
         }
-        function onchange() {
-            const hsv = rgb2hsv(255, 0, 0);
-            const changedHsv = { h: Math.floor(hsv.h + Number(range!.value) * 360) % 360, s: hsv.s, v: hsv.v };
-            const rgb = hsv2rgb(changedHsv.h, changedHsv.s, changedHsv.v);
-            if (value && range) value.textContent = `H(${Number(range.value).toFixed(2)}) RGB(${rgb.r}, ${rgb.g}, ${rgb.b})`;
+        if (range) range.onchange = () => {
+            view.h = Number(range.value);
+            draw();
+        };
+        canvas.onmousedown = (e: MouseEvent) => view.dragging = true;
+        canvas.onmouseleave = canvas.onmouseup = (e: MouseEvent) => view.dragging = false;
+        canvas.onmousemove = (e: MouseEvent) => {
+            if (view.dragging) {
+                view.x += e.movementX / X;
+                while (view.x < 0) view.x += X;
+                while (view.x >= X) view.x -= X;
+                view.y += e.movementY / Y;
+                if (view.y < 0) view.y = 0;
+                else if (view.y >= Y) view.y = Y - 1;
+                draw();
+            }
+        };
+        canvas.onwheel = (e: WheelEvent) => {
+            view.z += e.deltaY / 10;
             draw();
         }
-        if (range) range.onchange = onchange;
-        onchange();
+        draw();
     }
     function rgb2hsv(r: number, g: number, b: number) {
         const max = Math.max(r, g, b);
@@ -207,9 +277,5 @@ gl_FragColor = vec4(hsv2rgb(hsv), 1.0);
     }
 
     window.onerror = (e) => document.querySelector("body")!.textContent = e.toString();
-    window.onload = () => {
-        const image = new Image();
-        image.src = `${view.name}/x-${view.x % 7}_${view.name}_1.${view.y % 17 + 1}.png`;
-        image.onload = () => render(image);
-    }
+    window.onload = () => render();
 })();
